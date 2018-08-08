ee280f91b7b5d315b59ba1ad930dfcb2f94777b1
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-08 16:42:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
-var playlist = { firstSong: 'Have It All'}
+var playlist = { jasonMraz: 'Have It All'}
+
+function updatePlaylist(playlist, artist, song){
+  Object.assign({}, artist, song)
+  return playlist
+}
